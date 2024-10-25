ea519875a8ae09e55b20b237d8f3e9db13049943
Jonathan Hanekom <jonathan.david.hanekom@gmail.com>
2024-10-25 15:16:37 +0200

    ..

--- gui.py
+++ gui.py
@@ -1,14 +1,15 @@
-import customtkinter
+import customtkinter as ctk
+from api import *
 
 def button_callback():
     print("button pressed")
 
-app = customtkinter.CTk()
+app = ctk.CTk()
 app.title("my app")
 app.geometry("1000x500")
 app.grid_columnconfigure(2, weight=1)
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button = ctk.CTkButton(app, text="my button", command=button_callback)
 button.grid(row=0, column=0, padx=20, pady=20)
 
 app.mainloop()
